0d64c9b5dd986cab4736c01d60aa1e90d2860f1e
Michiel <git@fastfox.nl>
2013-11-27 14:47:54 +0100

    bbob included, fitness deleted and limits set

--- ABC.m
+++ ABC.m
@@ -1,4 +1,5 @@
-function [x,fx,fnx] = ABC(CS,D,its)
+%function [x,fx,fnx] = ABC(CS,D,its)
+function [x,fx] = ABC(CS,D,its)
 % [x,fx,fnx] = ABC(its)
 % Artificial Bee Colony optimization algorithm
 % Runs the algorithms with colony size CS for the
@@ -6,7 +7,24 @@ function [x,fx,fnx] = ABC(CS,D,its)
 %    X: the best solution found
 %    FX: the value of f(X) (function to be optimized)
 %    FNX: the fitness value of X
-    
+
+		% BBOB settings
+		datapath = '.';
+		opt.algName = 'ABC';
+		opt.comments = 'PUT MORE DETAILED INFORMATION, PARAMETER SETTINGS ETC';
+		func_id = 1;
+		instance_id = 1;
+		fgeneric('initialize', func_id, instance_id, datapath, opt);
+%    FUNC_ID is an integer setting the fitness function to be evaluated.
+%    INSTANCE_ID is the instance of the fitness function. Optimal and
+%      target function value and the applied rotation(s) are different
+%      for each INSTANCE_ID.
+
+		% So, we have multiple functions, which have an ID. There are multiple instances of functions, with difference parameters. 
+		% The dimensions are automaticly adjusted to the length of the solution given
+
+		
+
     n_employed = CS / 2; %number of empoyed bees
     n_onlookers = n_employed; %number of onlookers
     L = CS * D / 2; %limit
@@ -15,9 +33,11 @@ function [x,fx,fnx] = ABC(CS,D,its)
     counter = zeros(1,n_employed);
     
     for i = 1:n_employed
-        fit(i) = fitness(X(:,i));
+        %fit(i) = fitness(X(:,i));
+        fit(i) = f(X(:,i));
     end
-    [M,I] = max(fit);
+    %[M,I] = max(fit);
+		[M,I] = min(fit);
     x_best = X(:,I);
     
     for it = 1:its
@@ -28,7 +48,10 @@ function [x,fx,fnx] = ABC(CS,D,its)
             k = ceil(rand() * n_employed);
             V = X(:,i);
             V(j) = X(j,i) + phi(X(j,i) - X(j,k));
-            if (fitness(V) > fitness(X(:,i)))
+						V(j) = max(-5, min(5, X(j,i) + phi(X(j,i) - X(j,k))));
+            %if (fitness(V) > fitness(X(:,i)))
+            %if (fitness(V) < fitness(X(:,i)))
+            if (f(V) < f(X(:,i)))
                 X(:,i) = V;
                 counter(i) = 0;
             else
@@ -47,7 +70,10 @@ function [x,fx,fnx] = ABC(CS,D,its)
             k = ceil(rand() * n_employed);
             V = X(:,i);
             V(j) = X(j,i) + phi(X(j,i) - X(j,k));
-            if (fitness(V) > fitness(X(:,i)))
+						V(j) = max(-5, min(5, X(j,i) + phi(X(j,i) - X(j,k))));
+            %if (fitness(V) > fitness(X(:,i)))
+            %if (fitness(V) < fitness(X(:,i)))
+            if (f(V) < f(X(:,i)))
                 X(:,i) = V;
                 counter(i) = 0;
             else
@@ -63,30 +89,39 @@ function [x,fx,fnx] = ABC(CS,D,its)
         end
         
         for i = 1:n_employed
-            fit(i) = fitness(X(:,i));
+            %fit(i) = fitness(X(:,i));
+            fit(i) = f(X(:,i));
         end
-        [M,I] = max(fit);
-        if (fitness(X(:,I)) > fitness(x_best))
+        %[M,I] = max(fit);
+        [M,I] = min(fit);
+        %if (fitness(X(:,I)) > fitness(x_best))
+        %if (fitness(X(:,I)) < fitness(x_best))
+        if (f(X(:,I)) < f(x_best))
             x_best = X(:,I); %best found so far
         end
     end
     
     x = x_best;
     fx = f(x);
-    fnx = fitness(x);
+    %fnx = fitness(x);
+
+
 end
 
 % minimization problem: returned function value low -> high fitness
 function fit = fitness(x)
-    if (f(x) < 0) 
-        fit = 1 + abs(f(x)); 
-    else
-        fit = 1 / (1 + f(x));
-    end
+    %if (f(x) < 0) 
+    %    fit = 1 + abs(f(x)); 
+    %else
+    %    fit = 1 / (1 + f(x));
+    %end
+		fit = x;
+		%fit = fgeneric(x);
 end
 
 function y = f(x)
-    y = sum(x.^2);
+    %y = sum(x.^2);
+		y = fgeneric(x);
 end
 
 function y = phi(x)
