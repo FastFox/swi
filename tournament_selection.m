e68a70054bd4807775c10b54924a398f4f036246
Lai-yee <lyliu92@gmail.com>
2013-11-30 18:41:41 +0100

    fixed: now selects minimum

--- tournament_selection.m
+++ tournament_selection.m
@@ -9,6 +9,8 @@ function employed_index = tournament_selection(employed_fitness)
         candidates_index(i) = randi(mu);
         candidates_fitness(i) = employed_fitness(candidates_index(i));   
     end
-    [max_fitness, max_index] = max(candidates_fitness); % max_index is index of candidates vector (and not original vector)
-    employed_index = candidates_index(max_index);
+    %[max_fitness, max_index] = max(candidates_fitness); % max_index is index of candidates vector (and not original vector)
+    %employed_index = candidates_index(max_index);
+    [min_fitness, min_index] = min(candidates_fitness); 
+    employed_index = candidates_index(min_index);
 end
