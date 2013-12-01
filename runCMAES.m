c6e16b1c0d6eae94d35e94e60c2c6b215cb416d5
Michiel <git@fastfox.nl>
2013-12-01 13:13:27 +0100

    batch

--- runCMAES.m
+++ runCMAES.m
@@ -1,9 +1,26 @@
+opt.algName = 'CMAES';
+
 opts.LBounds = -5;
 opts.UBounds = 5;
 opts.Restart = 3;
 opts.maxFunEvals = 10000;
-fgeneric('initialize', 1, 1, '.', opts);
-X = cmaes('fgeneric', 10*rand(10, 1) - 5, 5, opts)
-fgeneric('fbest')
-fgeneric('ftarget')
-fgeneric('finalize')
+
+results = cell(1, 20);
+for c = 1:20
+	results{c} = zeros(20, 10);
+end
+
+for funcID = [1 3 6 10 15 20];
+	disp(['Func: ', num2str(funcID)]);
+	for dim = [5 10 20];
+		disp(['    Dim ', num2str(dim)]);
+		for i = 1:10
+			fgeneric('initialize', funcID, 1, '.', opt);
+			[xmin, fmin, counteval, stopflag] = cmaes('fgeneric', 4 * rand(dim, 1) - 4, rand(dim, 1));
+			results{funcID}(dim, i) = fgeneric('fbest');
+			fgeneric('finalize');
+		end
+		%[min(results);	max(results); mean(results); std(results)]
+	end
+end
+
